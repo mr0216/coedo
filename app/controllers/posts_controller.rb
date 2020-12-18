0943e5fdacb9c59c3250673dc4017a5829f0275b
mr0216 <nnr0216@gmail.com>
2020-12-18 17:36:40 +0900

    ヘッダー編集

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except:[:index,:show]
+  before_action :set_post, only: [:edit, :show]
 
   def index
     @posts = Post.all
@@ -20,11 +21,25 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to user_path
+    else
+      render :edit
+    end
+  end
+
   def show
-    @post = Post.find(params[:id])
   end
 
   private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:title, :category_id, :text, :image).merge(user_id: current_user.id)
