c541b909f40684dc04b1df182d5db0baf8482055
mr0216 <nnr0216@gmail.com>
2020-12-21 14:01:34 +0900

    投稿編集機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,8 +25,9 @@ class PostsController < ApplicationController
   end
 
   def update
+    @post = Post.find(params[:id])
     if @post.update(post_params)
-      redirect_to user_path
+      redirect_to post_path
     else
       render :edit
     end
