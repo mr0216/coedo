f28b84d17e0e604dcf19178a34cdea3402978822
mr0216 <nnr0216@gmail.com>
2020-12-22 16:31:09 +0900

    ユーザー一覧実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.all
+    @user = User.all.order(id: 'ASC')
+  end
+
   def show
     @user = User.find(params[:id])
     @posts = @user.posts
