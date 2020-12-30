7b174e4dbd51f967faa4700113ef07c9627306b6
mr0216 <nnr0216@gmail.com>
2020-12-30 23:30:26 +0900

    ユーザー編集機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,26 @@ class UsersController < ApplicationController
     @posts = @user.posts
     @posts = Post.order('created_at DESC')
   end
+
+  def edit
+    @user = User.find(params[:id])
+    unless @user.id == current_user.id
+      redirect_to action: :index
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if current_user.update(user_params)
+      redirect_to user_path(current_user.id)
+      sign_in(current_user, bypass: true)
+    else
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:nickname,:email, :password, :password_confirmation, :category_id, :introduction, :url, :business_hour, :address, :latitude,:longitude, :phone_number, :image)
+  end
 end
