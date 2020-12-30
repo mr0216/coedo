2964b6911241b8d26a2b7a38115784c992dacda7
mr0216 <nnr0216@gmail.com>
2020-12-30 23:56:41 +0900

    ユーザー削除機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,21 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, except:[:index,:show]
+  before_action :set_user, only: [:show, :edit, :update, :destroy ]
+  before_action :contributor_confirmation, only: [:edit, :destroy]
 
   def index
     @users = User.all.order(id: :DESC).page(params[:page]).per(5)
   end
 
   def show
-    @user = User.find(params[:id])
     @posts = @user.posts
     @posts = Post.order('created_at DESC')
   end
 
   def edit
-    @user = User.find(params[:id])
-    unless @user.id == current_user.id
-      redirect_to action: :index
-    end
   end
 
   def update
-    @user = User.find(params[:id])
     if current_user.update(user_params)
       redirect_to user_path(current_user.id)
       sign_in(current_user, bypass: true)
@@ -27,8 +24,26 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    if @user.destroy
+      redirect_to root_path
+    else
+      render root_path
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:nickname,:email, :password, :password_confirmation, :category_id, :introduction, :url, :business_hour, :address, :latitude,:longitude, :phone_number, :image)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def contributor_confirmation
+    unless @user.id == current_user.id
+      redirect_to action: :index
+    end
   end
 end
